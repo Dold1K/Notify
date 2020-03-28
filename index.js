e4292737e6c4786bac47667b21775b967689e88c
Dold1K <zortapex@mail.ru>
2020-03-28 23:33:59 +0300

    v0.2.1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const botconfig = require("./botconfig.json");
 
 const token = (botconfig.token)
 const prefix = (botconfig.prefix)
-const commands = ["n!ping", " n!help", " n!e"]
+const commands = ["n.ping!", " n.help", " n.profile"]
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -14,30 +14,31 @@ client.on("ready", () => {
 });
 //ping => pong
 client.on("message", msg => {
-  if (msg.content === prefix + "ping") {
+  //ping
+  if (msg.content === prefix + "ping!") {
     msg.channel.send("pong!");
-  }
-  //pin text
-  if (msg.content === "test" + pinText) {
-    msg.channel.pin(pinText);
-    var pinText = msg.content.slice(4);
   }
   //help commands
   if (msg.content === prefix + "help") {
     msg.channel.send(`Вот мои комманды: ${commands}`);
   }
   //embedMessage
-  if (msg.content === prefix + "e") {
+  if (msg.content === prefix + "profile") {
     let user = new Discord.User(client)
-    user = msg.author
-    const embed = new Discord.RichEmbed()
-      .setTitle(`Профиль, ${user.tag}`)
+    user = msg.author  
+    let url = msg.user.avatarUrl();
+    const profile = new Discord.RichEmbed()
+      .setTitle(`Профиль ${user.tag}`)
       .setColor("0e69f0")
-      .setDescription("Прив")
-      
-    msg.channel.send(embed);
+      .setThumbnail(url)
+      .setDescription()
+      //.setA()
+    msg.channel.send(profile);
+    if (msg.content === 'what is my avatar') {
+      // Send the user's avatar URL
+      msg.reply(msg.author.displayAvatarURL());
+    }
   }
-  
 });
 /*client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.cache.find(ch => ch.id === "log");
@@ -45,7 +46,3 @@ client.on("message", msg => {
   channel.send(`Welcome to the server, ${member}`);
 });*/
 client.login(token);
-
-function newFunction(msg) {
-  return msg.author;
-}
